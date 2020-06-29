9bd7662f8f034a9a727b128e7e508cfcb4fe07eb
sponja-coursera <67340041+sponja-coursera@users.noreply.github.com>
2020-06-29 16:31:25 -0300

    Update cachematrix.R
    Created the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a matrix with it's reverse cached so that it doesn't have to calculate it again later
 
-## Write a short comment describing this function
-
+## Creates a matrix with it's reverse cached
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## Calculates the inverse of a matrix if it's not cached already
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  
+  return(m)
 }
